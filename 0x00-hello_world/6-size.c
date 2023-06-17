d01c4e370336bd2a77ca4ad20228f08f26e4e5e6
Martin Rota <joelrota5@gmail.com>
2023-06-17 02:13:34 -0700

    perform Hello_world tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -13,6 +12,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float int: %zu byte(s)\n", sizeof(float));
-
 	return (0);
 }
