8749d4f147340788317a77ffe2e3b5bbd51b3d89
Martin Rota <joelrota5@gmail.com>
2023-06-18 10:44:34 -0700

    perform hello_world task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-int main () { puts ("Programming is like building a multilingual puzzle"); return 0; }
+
+/**
+ *main - Entry Point
+ *Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	puts ("Programming is like building a multilingual puzzle");
+	return 0;
+}
