3bfb4c864957bf134207c33cff9b944ec9fd9003
Martin Rota <joelrota5@gmail.com>
2023-06-21 18:14:15 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		letter = 'a';
+
+		while (letter <= 'z')		
+		{
+			_putchar(letter);
+			letter++;
+		}
+		
+		_putchar('\n');
+	}
+}
