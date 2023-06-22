41feadadd95fc136552b7deba62dd6d6ecc88acb
Martin Rota <joelrota5@gmail.com>
2023-06-21 18:21:06 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,16 +10,15 @@ void print_alphabet_x10(void)
 	char letter;
 	int i;
 
-	for (i = 1; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		letter = 'a';
 
-		while (letter <= 'z')		
+		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
 		}
-		
 		_putchar('\n');
 	}
 }
