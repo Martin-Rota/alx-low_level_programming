2763bb68e407b033c9d7d594d58eb8619a813c27
Martin Rota <joelrota5@gmail.com>
2023-06-21 19:09:32 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -41,4 +41,12 @@ int _islower(int c);
 
 int _isalpha(int c);
 
+/**
+* print_sign - prints the sign of a number
+* @n: the number to check
+*
+* Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ */
+int print_sign(int n);
+
 #endif
