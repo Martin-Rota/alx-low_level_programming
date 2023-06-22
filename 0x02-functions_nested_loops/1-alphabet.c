fc893aaa50bd3620dd5de908b117480b13c4cf04
Martin Rota <joelrota5@gmail.com>
2023-06-21 17:53:43 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prints_the_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		alpha++;
+	}
+	_putchar('\n');
+}
