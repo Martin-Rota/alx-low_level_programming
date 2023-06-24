e5dc0a1e02dd509f01bcbe75f80195758c966897
Martin Rota <joelrota5@gmail.com>
2023-06-24 04:30:49 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
