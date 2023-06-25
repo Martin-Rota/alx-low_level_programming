6ed85b1ed23a541deb0f03fdadb894ca905bfca9
Martin Rota <joelrota5@gmail.com>
2023-06-25 05:21:00 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,13 +5,13 @@
  *
  * returns 1, 0 if lowercase
  *
- * @c: this is the entry
+ * @c: entry to be checked
  **/
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	else
+
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * largest_prime_factor - Finds the largest prime factor
+ * @n: The number to find the largest prime factor of
+ *
+ * Return: The largest prime factor of the number
+ */
+
+unsigned long largest_prime_factor(unsigned long n)
+{
+	unsigned long largest_factor = 2;
+
+	while (n > largest_factor)
+	{
+		if (n % largest_factor == 0)
+		{
+			n /= largest_factor;
+		}
+		else
+		{
+			largest_factor++;
+		}
+	}
+
+	return largest_factor;
+}
+
+int main(void)
+{
+	unsigned long number = 612852475143;
+	unsigned long largest_prime = largest_prime_factor(number);
+
+	printf("%lu\n", largest_prime);
+
+	return 0;
+}
