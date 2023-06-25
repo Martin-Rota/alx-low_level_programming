2a25fd106ea82cb9768ec0655f31181c6d4ca69d
Martin Rota <joelrota5@gmail.com>
2023-06-25 01:39:40 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,13 @@
  * _isupper - Checks if uppercase and returns 1
  * @c: this is the entry
  * Returns 0 otherwise
- *
  **/
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
+
 	else
 		return (0);
 }
