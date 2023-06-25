14b231630734269843a169ab2fd296b7dc69705c
Martin Rota <joelrota5@gmail.com>
2023-06-25 04:35:43 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,16 @@ void print_square(int size)
 {
 	int row, column;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (row = 0; row < size; row++)
-		{
-			for (column = 0; column < size; column++)
-				_putchar(35);
-
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+			_putchar(35);
+
+		_putchar('\n');
+	}
 }
