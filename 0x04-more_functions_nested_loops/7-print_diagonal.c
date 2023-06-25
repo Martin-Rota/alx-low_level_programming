6cdaee93b4a78830c19511925772cb2fce1a7080
Martin Rota <joelrota5@gmail.com>
2023-06-25 02:05:30 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@ void print_diagonal(int n)
 	if (n > 0)
 		for (draw = 0; draw < n; draw++)
 		{
-			for (space = 1; space < draw; space++)
+			for (space = 0; space < draw; space++)
 				_putchar(' ');
 
 			_putchar('\\');
