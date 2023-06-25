9065ad375deefc2385aae4a0285599dbc9c1ffa6
Martin Rota <joelrota5@gmail.com>
2023-06-25 04:08:21 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,13 @@ void print_triangle(int size)
 	if (size < 1)
 	{
 		_putchar('\n');
-		return;
 	}
-	for (replay = 1; replay <= size; replay++)
+
+	for (replay = 0; replay < size; replay++)
 	{
-		for (count = size - replay; count > 0; count--)
+		for (count = size - replay; count > 1; count--)
 			_putchar(' ');
-		for (count = replay; count > 0; count--)
+		for (count = replay; count >= 0; count--)
 			_putchar('#');
 		_putchar('\n');
 	}
