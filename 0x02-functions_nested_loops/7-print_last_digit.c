e2cd3326948d39e801ca5f4d19c57462174cf0ab
Martin Rota <joelrota5@gmail.com>
2023-06-25 13:17:02 -0700

    nested loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,19 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n *= -1;
+	{
+		if (n == INT_MIN)
+		{
+			_putchar('8');
+			return (8);
+		}
+		n = -n;
+	}
 
 	last_digit = n % 10;
+
 	_putchar(last_digit + '0');
+	_putchar('\n');
 
 	return (last_digit);
 }
