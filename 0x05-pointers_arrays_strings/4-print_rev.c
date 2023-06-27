9b6807404c83e81fe796f52ae35c22e051d2538c
Martin Rota <joelrota5@gmail.com>
2023-06-26 19:31:41 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,12 @@
  * _print_rev_recursion - Prints a string in reverse followed by a new line.
  * @s: Pointer to the string to be printed.
  */
-void _print_rev_recursion(char *s)
+
+void _print_rev(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s + 1);
+		_print_rev(s + 1);
 		_putchar(*s);
 	}
 	else
