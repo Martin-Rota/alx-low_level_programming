a904edb6e51c5eb3837955a918d42fef1a924193
Martin Rota <joelrota5@gmail.com>
2023-06-29 12:04:08 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,18 @@
  *
  * Return: Pointer to the resulting string dest.
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
 	int i;
 
-	/* Find the length of the destination string */
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	/* Append the source string to the destination string */
 	for (i = 0; src[i] != '\0'; i++)
 		dest[dest_len++] = src[i];
 
-	/* Add the terminating null byte */
 	dest[dest_len] = '\0';
 
 	return (dest);
