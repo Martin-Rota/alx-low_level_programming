21bbc258f55f3235243522b9e09bae6e794211f4
Martin Rota <joelrota5@gmail.com>
2023-06-29 20:01:15 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,29 +6,20 @@
  *
  * Return: Pointer to the resulting encoded string.
  */
-
 char *rot13(char *str)
-
 {
 	int i, j;
-	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	/* Iterate through each character in the string */
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		/* Check if the character is an alphabet */
-		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+		for (j = 0; alphabet[j] != '\0'; j++)
 		{
-			/* Find the corresponding index in the alphabet array */
-			for (j = 0; alpha[j] != '\0'; j++)
+			if (str[i] == alphabet[j])
 			{
-				if (str[i] == alpha[j])
-				{
-					/* Replace the character with its ROT13 equivalent */
-					str[i] = rot13[j];
-					break;
-				}
+				str[i] = rot13[j];
+				break;
 			}
 		}
 	}
