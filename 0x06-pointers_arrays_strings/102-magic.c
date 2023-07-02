c1ea4c08728b881345793c779e3aa6f063587896
Martin Rota <joelrota5@gmail.com>
2023-07-02 04:55:27 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Prints an integer
+ * @n: The number to be printed
+ *
+ * Description: This function prints an integer using the _putchar function.
+ * It handles both positive and negative numbers.
+ */
+
+void print_number(int n)
+{
+	unsigned int divisor, digit, positive = n;
+	double tens_place = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (tens_place <= positive)
+			tens_place *= 10;
+		divisor = tens_place / 10;
+
+		while (divisor >= 1)
+		{
+			digit = positive / divisor;
+			_putchar(digit + '0');
+			positive = positive - (divisor * digit);
+			divisor /= 10;
+		}
+	}
+
+	/* Additional line of code */
+	printf("a[2] = %d\n", 'a'[2]);
+}
