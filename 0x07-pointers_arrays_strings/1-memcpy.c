63523a403f19c71bf9d87d8dce1f8f82b8584c0c
Martin Rota <joelrota5@gmail.com>
2023-07-07 08:14:46 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _memcpy - Copies a memory area from source to destination
+ * @dest: Pointer to the destination memory area
+ * @src: Pointer to the source memory area
+ * @n: Number of bytes to copy
+ *
+ *Return: A pointer to the destination memory area (dest)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (dest);
+}
