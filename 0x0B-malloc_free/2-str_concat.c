860443aea326146922d7ea0931f2c338eea4e6d2
Martin Rota <joelrota5@gmail.com>
2023-07-12 03:25:30 -0700

    Static Lib

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	size_t s1_len, s2_len;
+	char *concat;
 
 	if (s1 == NULL)
 	{
@@ -28,7 +29,8 @@ char *str_concat(char *s1, char *s2)
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
 
-	char *concat = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	concat = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+
 	if (concat == NULL)
 	{
 		return (NULL);
