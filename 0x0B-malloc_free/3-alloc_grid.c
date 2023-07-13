61dd970559bc968abe46b6436774d740440125dd
Martin Rota <joelrota5@gmail.com>
2023-07-12 19:27:03 -0700

    Static Lib

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,8 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-		
+	int i, j;
+	
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
@@ -24,7 +25,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	int i, j;
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * width);
