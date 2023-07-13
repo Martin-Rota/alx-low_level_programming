c5d1745b6911dbd982aa93a47a93d6facb2e5d64
Martin Rota <joelrota5@gmail.com>
2023-07-12 19:55:21 -0700

    Static Lib

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,17 +13,18 @@
 
 char *argstostr(int ac, char **av)
 {
+	int total_length;
+	int i, j, k;
+	char *concatenated_str;
+
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-	int total_length;
-	int i, j, k;
-	char *concatenated_str;
-
 	/* Calculate the total length of the concatenated string */
 	total_length = 0;
+
 	for (i = 0; i < ac; i++)
 	{
 		total_length += strlen(av[i]) + 1; /* +1 for '\n' */
