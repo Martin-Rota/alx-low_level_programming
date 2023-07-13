5083b7054a7cb91d4b55fd30cf0eab5b50ebf263
Martin Rota <joelrota5@gmail.com>
2023-07-12 19:36:20 -0700

    Static Lib

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * free_grid - Frees the memory allocated for a 2-dimensional grid.
@@ -8,12 +9,13 @@
 void free_grid(int **grid, int height)
 
 {
+	int i;
 	if (grid == NULL || height <= 0)
 	{
-		return();
+		return;
 	}
 
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
