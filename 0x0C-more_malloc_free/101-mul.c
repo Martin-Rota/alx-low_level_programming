05e86a808385318dd3792e7093422c5be7cf6a7e
Martin Rota <joelrota5@gmail.com>
2023-07-14 22:56:56 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,66 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * validate_input - Validate if string contains only digits
- * @num: The string to be validated
+ * _isdigit - Check if string consists of only digits.
+ * @str: The string to be checked
  *
- * Return: 1 if the string contains only digits, 0 otherwise
+ * Return: 1 if the string consists of only digits, 0 otherwise.
  */
-
-int validate_input(char *num)
+int _isdigit(char *str)
 {
 	int i;
 
-	i = 0;
-	while (num[i] != '\0');
-	i++;
-
-	if (!isdigit(num[i]))
-		return (0);
-
-	else
-		return (1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+	return (1);
 }
 
 /**
- * multiply_numbers - Multiply two numbers and print the result
- * @num1: The first number to be multiplied
- * @num2: The second number to be multiplied
+ * _strlen - Calculate length of string
+ * @str: The string to be measured.
+ *
+ * Return: The length of string.
  */
-
-void multiply_numbers(char *num1, char *num2)
+int _strlen(char *str)
 {
-	int result;
+	int len;
 
-	result = atoi(num1) * atoi(num2);
-	printf("%d\n", result);
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
 }
 
 /**
- * main - Entry point of the program
+ * _multiply - Multiply two positive numbers and print the result.
+ * @num1: The first number to be multiplied.
+ * @num2: The second number to be multiplied.
+ */
+void _multiply(char *num1, char *num2)
+{
+	int len1;
+	int len2;
+	int len_result;
+	int *result;
+	int i, j, carry, num;
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len_result = len1 + len2;
+	result = calloc(len_result, sizeof(int));
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			num = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+			result[i + j + 1] = num % 10;
+			carry = num / 10;
+		}
+		result[i] += carry;
+	}
+
+	i = 0;
+	while (i < len_result - 1 && result[i] == 0)
+		i++;
+
+	for (; i < len_result; i++)
+		printf("%d", result[i]);
+	printf("\n");
+
+	free(result);
+}
+
+/**
+ * main - Entry point of program
  * @argc: The number of command line arguments
  * @argv: Array of command line arguments
  *
  * Return: 0 on success, 98 on error
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	if (!validate_input(argv[1]) || !validate_input(argv[2]))
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	multiply_numbers(argv[1], argv[2]);
+	_multiply(argv[1], argv[2]);
 
 	return (0);
 }
