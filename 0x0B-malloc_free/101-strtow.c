87cd448498da57bbd1c21ce95b93f008ff412e0d
Martin Rota <joelrota5@gmail.com>
2023-07-14 23:37:51 -0700

    more malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,46 +2,44 @@
 #include <stdio.h>
 
 /**
- * is_space - Checks if character is space character
+ * is_space - Checks if character is a space character
  * @c: Character to check
  *
- * Return: 1 if the character is space character, 0 otherwise
+ * Return: 1 if the character is a space character, 0 otherwise
  */
 int is_space(char c)
 {
-    return (c == ' ' || c == '\t' || c == '\n');
+	return (xc == ' ' || c == '\t' || c == '\n');
 }
 
 /**
- * count_words - Counts number of words in string
+ * count_words - Counts the number of words in a string
  * @str: String to count words from
  *
  * Return: Number of words in the string.
  */
-
 int count_words(char *str)
 {
-    int count;
-    int i;
-    
-    count = 0;
-    i = 0;
-	    
-    while (str[i] != '\0')
-    {
-        if (!is_space(str[i]) && (is_space(str[i + 1]) || str[i + 1] == '\0'))
-            count++;
-        i++;
-    }
+	int count;
+	int i;
 
-    return count;
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!is_space(str[i]) && (is_space(str[i + 1]) || str[i + 1] == '\0'))
+			count++;
+		i++;
+	}
+
+	return (count);
 }
 
 /**
  * strtow - Split string into words
  * @str: String to split
  *
- * Return: Pointer to array of strings(words)
+ * Return: Pointer to array of strings (words)
  *         Returns NULL if str == NULL or str == "".
  *         Returns NULL on failure.
  */
@@ -49,49 +47,49 @@ char **strtow(char *str)
 {
 	int words, i, j, k, l;
 	char **result;
+	
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-    if (str == NULL || *str == '\0')
-        return NULL;
+	words = count_words(str);
+	result = malloc((words + 1) * sizeof(char *));
+	if (result == NULL)
+		return (NULL);
 
-    words = count_words(str);
-    result = malloc((words + 1) * sizeof(char *));
-    if (result == NULL)
-        return NULL;
+	i = 0;
+	j = 0;
+	k = 0;
 
-    i = 0;
-    j = 0;
-    k = 0;
+	while (str[i] != '\0')
+	{
+		if (!is_space(str[i]))
+		{
+			j = i;
+			while (!is_space(str[j]) && str[j] != '\0')
+				j++;
 
-    while (str[i] != '\0')
-    {
-        if (!is_space(str[i]))
-        {
-            j = i;
-            while (!is_space(str[j]) && str[j] != '\0')
-                j++;
+			result[k] = malloc((j - i + 1) * sizeof(char));
+			if (result[k] == NULL)
+			{
+				while (k > 0)
+					free(result[--k]);
+				free(result);
+				return (NULL);
+			}
 
-            result[k] = malloc((j - i + 1) * sizeof(char));
-            if (result[k] == NULL)
-            {
-                while (k > 0)
-                    free(result[--k]);
-                free(result);
-                return NULL;
-            }
+			l = 0;
+			while (i < j)
+			{
+				result[k][l] = str[i];
+				i++;
+				l++;
+			}
+			result[k][l] = '\0';
+			k++;
+		}
+		i++;
+	}
 
-	    l = 0;
-            while (i < j)
-            {
-                result[k][l] = str[i];
-                i++;
-                l++;
-            }
-            result[k][l] = '\0';
-            k++;
-        }
-        i++;
-    }
-
-    result[k] = NULL;
-    return result;
+	result[k] = NULL;
+	return (result);
 }
