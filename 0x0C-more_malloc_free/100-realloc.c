dbf77c0cfd3767500fe4b74ba18b5a1201d27dee
Martin Rota <joelrota5@gmail.com>
2023-07-14 21:49:57 -0700

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new_ptr;
 	unsigned int min_size;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL))
 	{
 		free(ptr);
 		return (NULL);
@@ -34,6 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	min_size = (old_size < new_size) ? old_size : new_size;
 	memcpy(new_ptr, ptr, min_size);
+
 	free(ptr);
 
 	return (new_ptr);
