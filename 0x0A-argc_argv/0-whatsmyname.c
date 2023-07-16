da70e15f8c32f06b1cb2a60d4944298dcf803b1d
Martin Rota <joelrota5@gmail.com>
2023-07-16 07:38:49 -0700

    argc,argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point of the program
- * @argc: Number of command-line arguments
  * @argv: Array of command-line arguments
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(char *argv[])
+
 {
 	printf("%s\n", argv[0]);
 
