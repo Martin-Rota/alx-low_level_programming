be5232a0ac91e70e57009fa8f73cd7436b29758a
Martin Rota <joelrota5@gmail.com>
2023-07-19 21:10:30 -0700

    4-new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/** new_dog - Creates a new dog_t structure.
+ * 
+ * @name Pointer to the name of the new dog.
+ * @age Age of the new dog.
+ * @owner Pointer to the owner's name of the new dog
+ *
+ * Return: Pointer to the new dog_t structure
+ *         Null if memory allocation fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+        dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = (char *)malloc(strlen(name) + 1);
+	if (new_dog->name == NULL)
+		free (new_dog);
+		return (NULL);
+
+	stropy(new_dog->name, name);
+
+new_dog->owner = (char *)malloc(strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+		free (new_dog->name);
+		free (new_dog);
+		return (NULL);
+
+	stropy(new_dog->owner, owner); 
+
+	new_dog->age = age;
+
+	return (new_dog);
+}
