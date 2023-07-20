fd302125ca9b43734d30d74c421df314b67641f6
Martin Rota <joelrota5@gmail.com>
2023-07-20 02:29:19 -0700

    Betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,9 +6,9 @@
 
 /**
  * new_dog - Creates a new dog_t structure.
- * @name Pointer to the name of the new dog.
- * @age Age of the new dog.
- * @owner Pointer to the owner's name of the new dog.
+ * @name: Pointer to the name of the new dog.
+ * @age: Age of the new dog.
+ * @owner: Pointer to the owner's name of the new dog.
  *
  * Return: Pointer to the new dog_t structure, NULL if allocation fails
  */
@@ -16,6 +16,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+
 	if (new_dog == NULL)
 		return (NULL);
 
