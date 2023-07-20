a674bfc58457ddd6c6ecc9fab9be568cb8489285
Martin Rota <joelrota5@gmail.com>
2023-07-19 19:42:35 -0700

    defines type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,11 +16,6 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-struct dog_t
-{
-        char *name;
-        float age;
-        char *owner;
-};
+typedef struct dog dog_t;
 
 #endif
