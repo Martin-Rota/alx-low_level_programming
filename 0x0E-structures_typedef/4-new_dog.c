f3ac7bbf185d7633344d0b9eefe3de3f083478d4
Martin Rota <joelrota5@gmail.com>
2023-07-20 02:13:52 -0700

    compilation errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,18 +20,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog->name = (char *)malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
+	{
 		free (new_dog);
 		return (NULL);
+	}
 
-	stropy(new_dog->name, name);
+	strcpy(new_dog->name, name);
 
 new_dog->owner = (char *)malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
+	{
 		free (new_dog->name);
 		free (new_dog);
 		return (NULL);
-
-	stropy(new_dog->owner, owner); 
+	}
+	
+	strcpy(new_dog->owner, owner); 
 
 	new_dog->age = age;
 
