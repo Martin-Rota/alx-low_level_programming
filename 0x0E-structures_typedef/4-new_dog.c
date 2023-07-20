91916600b0611f51748c2cb9f1156b502760bc2f
Martin Rota <joelrota5@gmail.com>
2023-07-20 02:24:49 -0700

    compilation errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**new_dog - Creates a new dog_t structure.
+/**
+ * new_dog - Creates a new dog_t structure.
  * @name Pointer to the name of the new dog.
  * @age Age of the new dog.
  * @owner Pointer to the owner's name of the new dog.
@@ -14,14 +15,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
 	new_dog->name = (char *)malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
-		free (new_dog);
+		free(new_dog);
 		return (NULL);
 	}
 
@@ -30,12 +31,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 new_dog->owner = (char *)malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
-		free (new_dog->name);
-		free (new_dog);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-	
-	strcpy(new_dog->owner, owner); 
+
+	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
 
