a02452cfc6f4a1d0432e30693285be7966b79cca
Martin Rota <joelrota5@gmail.com>
2023-07-24 14:01:43 -0700

    array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  * @array: Pointer to array.
  * @action: Pointer to function to be executed on each element of array
  * @size: Size of the array.
- * 
+ *
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
@@ -15,6 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array != NULL && action != NULL)
 	{
 		size_t i;
+		
 		for (i = 0; i < size; i++)
 		{
 		action(array[i]);
