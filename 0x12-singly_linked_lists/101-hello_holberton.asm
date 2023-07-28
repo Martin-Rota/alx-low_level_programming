4cfac13f89125ce6df0314628d390fe30a25af66
Martin Rota <joelrota5@gmail.com>
2023-07-28 16:11:39 -0700

    Assembly program

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,23 @@
+; 101-hello_holberton.asm
+
 section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
+	hello db "Hello, Holberton", 0
+	format db "%s", 10, 0
 
 section .text
-    global _start
+	global _start
 
 _start:
-    ; Call printf function
-    mov rdi, format    ; First argument: format string
-    mov rsi, hello     ; Second argument: address of hello string
-    xor rax, rax       ; Clear rax register (since printf is a variadic function)
-    call printf        ; Call printf function
+	; Call printf function
+	mov rdi, format    ; First argument: format string
+	mov rsi, hello     ; Second argument: address of hello string
+	xor rax, rax       ; Clear rax register (since printf is a variadic function)
+	call printf        ; Call printf function
 
-    ; Exit the program
-    mov rax, 60        ; Syscall number for exit
-    xor rdi, rdi       ; Exit status 0
-    syscall           ; Invoke syscall
+	; Exit the program
+	mov rax, 60        ; Syscall number for exit
+	xor rdi, rdi       ; Exit status 0
+	syscall           ; Invoke syscall
 
 section .bss
-    ; Empty BSS section to satisfy linking requirements
+	; Empty BSS section to satisfy linking requirements
