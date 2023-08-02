553ebdba532736e70cebd8015319d62b646f5115
Martin-Rota <martinrota5@gmail.com>
2023-08-02 07:51:58 -0700

    Task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,4 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct arg_t
+{
+    char specifier;
+    void (*func)(va_list);
+} arg_t;
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
 #endif
