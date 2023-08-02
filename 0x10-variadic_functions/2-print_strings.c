b7bb306190cb119c6491dd4cf103a4271e3b7379
Martin-Rota <martinrota5@gmail.com>
2023-08-02 06:54:32 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(args, const char *);
 		if (str == NULL)
 			printf("(nil)");
-
+		else
 			printf("%s", str);
 
 		if (separator != NULL && i < n - 1)
