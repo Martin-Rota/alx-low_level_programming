31a883af0ec3dc787006f0a06f3fa079f750f262
Martin-Rota <martinrota5@gmail.com>
2023-08-02 07:46:21 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,86 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - Print a character
+ * @args: arguments to print
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - Print an integer
+ * @args: argument to print
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Print a float
+ * @args: argument to print
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Print a string
+ * @args: argument to print
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+}
+
+/**
+ * print_all - Print anything
+ * @format: List of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *separator = "";
+	unsigned int i = 0, j = 0;
+	arg_t args_list[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	va_start(args, format);
+
+	while (format != NULL && format[i] != "\0")
+	{
+		j = 0;
+		while (args_list[j].specifier != '\0')
+		{
+			if (format[i] == args_list[j].specifier)
+			{
+				printf("%s", separator);
+				args_list[j].func(args);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
+#include "variadic_functions.h"
+
+/**
  * print_all - Prints anything
  * @format: List of types of arguments passed to the function
  */
