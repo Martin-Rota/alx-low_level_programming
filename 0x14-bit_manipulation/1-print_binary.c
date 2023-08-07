e4f10ac2fb901707d7f3e69d910281c98966547b
Martin-Rota <martinrota5@gmail.com>
2023-08-07 12:59:46 -0700

    Print Binary Code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,15 +8,15 @@
 void print_binary(unsigned long int n)
 {
 	unsigned int binary_digit, binary = 0;
-	int i = 1; /* places bit in right position */
+	int i = 1;
 
 	if (n > 0)
 	{
 		while (n > 0)
 		{
-			binary_digit = n % 2;
+			binary_digit = n & 1;
 			binary += binary_digit * i;
-			n /= 2;
+			n >>= 1;
 			i *= 10;
 		}
 	printf(" %u\n", binary);
