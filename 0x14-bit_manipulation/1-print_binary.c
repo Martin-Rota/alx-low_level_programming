4255e56e98bc220b8a0b89cc217f848f6019b712
Martin-Rota <martinrota5@gmail.com>
2023-08-07 12:33:25 -0700

    Print Binary Code

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**                                                                            
+ * print_binary - prints the binary representation of a number.
+ * @n: decimal number to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int binary_digit, binary = 0;
+	int i = 1; //places bit in right position
+
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			binary_digit = n % 2;
+			binary += binary_digit * i;
+			n /= 2;
+			i *= 10;
+		}
+	printf(" %lu\n", binary);
+	}
+
+	else
+		printf(" 0\n");
+}
