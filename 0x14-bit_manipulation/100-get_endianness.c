b507cc43cab0958881b1bcad750a54e8086e6263
Martin-Rota <martinrota5@gmail.com>
2023-08-09 15:06:25 -0700

    Initial commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* get_endianness -  checks endianness
+*
+* Return: 0 if big endian, 1 if little endian
+*/
+
+int get_endianness(void)
+{
+	unsigned int num = 8;
+	char *ptr = &num;
+
+	if (ptr == 0)
+		{
+		return (0);
+		}
+	else (ptr == 1)
+		{
+		return (1);
+		}
+}
