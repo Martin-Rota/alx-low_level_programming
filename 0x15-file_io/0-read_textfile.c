fb386c3a2330d6f4850288596705d807ed9d6820
Martin-Rota <martinrota5@gmail.com>
2023-08-11 17:22:22 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	count = 0;
 	while (count < letters && (c = fgetc(fd)) != EOF)
 	{
-		fputc(stdout, c);
+		fputc(c, stdout);
 		count ++;
 	}
 
