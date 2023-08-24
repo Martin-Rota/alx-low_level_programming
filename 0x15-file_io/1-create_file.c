e3c7fba2358c4ee3459fefe2ceabee14a98d3022
Martin-Rota <martinrota5@gmail.com>
2023-08-24 03:42:44 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL) 
+	if (text_content != NULL)
 	{
 		written_bytes = write(fd, text_content, strlen(text_content));
 		if (written_bytes == -1)
