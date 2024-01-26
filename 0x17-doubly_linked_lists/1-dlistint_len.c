b21ad5b1e17e0402f35f0d50c9de50f9ba24e742
Martin-Rota <martinrota5@gmail.com>
2024-01-25 19:16:47 -0800

    Task 1 first commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  returns the number of elements dlistint_t list.
+ * @h: A pointer to the head of the doubly linked list.
+ *
+ * Return: The number of elements in the list.
+ */
+ size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *current = h;
+    size_t node_count = 0;
+
+    while (current != NULL){
+        current = current->next;
+        node_count++;
+        }
+
+    return node_count;
+}
+
