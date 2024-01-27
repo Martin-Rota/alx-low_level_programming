61059495279c8bda394ae1c195208d71cb525101
Martin-Rota <martinrota5@gmail.com>
2024-01-27 02:41:22 -0800

    Fix kernel style error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint -  adds a new node at the beginning of a dlistint_t list
  * @head: A pointer to a pointer to the head of the doubly linked list.
- * @n - integer data stored in the n field
+ * @n: Integer data stored in the n field
  *
  * Return: The address of the new element, or NULL if it failed
  */
@@ -12,11 +12,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 	new_node->next = *head;
