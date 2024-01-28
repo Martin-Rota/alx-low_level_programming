260f288fe88f6ca32235a64362a0e11cb86ce3cf
Martin-Rota <martinrota5@gmail.com>
2024-01-28 13:10:11 -0800

    task 7 commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,6 +40,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
 
 	free(new_node);
 	return (NULL);
