56b3aa476d7ede44530782602f3a257fd8641f88
Martin-Rota <martinrota5@gmail.com>
2024-01-28 13:20:57 -0800

    task 7 commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	count = 0;
 
-	while ((idx != 0) && (current->next))
+	while (current != NULL)
 	{
 		if (count == idx)
 		{
