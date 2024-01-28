7db4acfdda423e7850f48a3e7c51a4aea85f108f
Martin-Rota <martinrota5@gmail.com>
2024-01-28 09:02:44 -0800

    task 4 first commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: A pointer to the head of the doubly linked list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (!head)
+		return;
+
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		free(current->prev);
+	}
+	free(current);
+}
