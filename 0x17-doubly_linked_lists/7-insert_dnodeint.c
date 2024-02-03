6e76c468d4fa5a487a21e85e34ef967d46a89212
Martin-Rota <martinrota5@gmail.com>
2024-02-03 04:13:36 -0800

    fix code to handle index that is out of range

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,23 +27,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	count = 0;
 
-	while (current != NULL && count < idx)
+	while (current != NULL && count < idx){
 		current = current->next;
 		count++;
-	
+	}
 	if (count < idx)
 		return (NULL);
 
 	count = 0;
 
 	while (current != NULL){
-		if (count == idx)
+		if (count == idx){
 			new_node->next = current;
 			new_node->prev = current->prev;
 			current->prev->next = new_node;
 			current->prev = new_node;
 			return (new_node);
 		}
+	}
 		current = current->next;
 		count++;
 	return (NULL);
