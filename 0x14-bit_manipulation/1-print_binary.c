c6f1a6586e73b99542e0b9e180e31c05a2fb9d33
Martin-Rota <martinrota5@gmail.com>
2024-03-14 07:09:28 +0300

    task 1 first commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: decimal number to be converted
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1, j;
+	int mask_length = 0;
+
+	for (j = n; j > 0; j >>= 1)
+	{
+		mask_length += 1;
+	}
+	mask_length -= 1;
+
+	if (n > 0)
+	{
+		mask = mask << mask_length;
+
+		while (mask > 0){
+			if (n & mask)
+				putchar('1');
+
+			else
+				putchar('0');
+
+			mask >>= 1;
+		}
+	}
+	else
+	{
+		putchar('0');
+	}
+}
