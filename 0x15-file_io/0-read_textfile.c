15f167531708650a33866a5afea46de2a2b031bf
Martin-Rota <martinrota5@gmail.com>
2024-04-27 23:44:12 +0300

    fix read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,48 @@
 #include "main.h"
 
 /**
-* read_textfile - Reads a text file and prints it to the POSIX standard output
-* @filename: File to read from
-* @letters: The number of letters it should read and print
-*
-* Return: actual number of letters it could read and print.
-*	: O if filename is null or if write fails.
-*/
-
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: File to read from
+ * @letters: The number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print;
+ *         0 if the file cannot be opened or read, or if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int c;
-	FILE *fd = NULL;
-	size_t count;
-	FILE *output_stream;
+    FILE *file;
+    ssize_t n_read = 0, n_written = 0;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+      return (0);
 
-	fd = fopen(filename, "r");
-	if (fd == NULL)
-		return (0);
+    file = fopen(filename, "r");
+    if (file == NULL)
+      return (0);
 
-	output_stream = to_stderr ? stderr : stdout;
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL) {
+        fclose(file);
+        return (0);
+    }
 
-	count = 0;
-	while (count < letters && (c = fgetc(fd)) != EOF)
-	{
-		fputc(c, output_stream);
-		count++;
-	}	
+    n_read = fread(buffer, sizeof(char), letters, file);
+    if (ferror(file)) {
+        free(buffer);
+        fclose(file);
+        return (0);
+    }
 
-	if (ferror(fd))
-	  {
-		fclose(fd);
-		return (0);
-	  }
+    n_written = fwrite(buffer, sizeof(char), n_read, stdout);
+    if (n_written < n_read) {
+        free(buffer);
+        fclose(file);
+        return (0);
+    }
 
-	fclose(fd);
-	return (count);
+    free(buffer);
+    fclose(file);
+    return (n_written);
 }
