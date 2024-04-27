25a7a9143ff91bf85a8d61c2affb0d32e3b531cd
Martin-Rota <martinrota5@gmail.com>
2024-04-27 22:39:25 +0300

    fix kernel style code errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,21 +5,20 @@
  * @b: binary
  * Return: unsigned int
  */
-unsigned int binary_to_uint(const char *b) 
+unsigned int binary_to_uint(const char *b)
 {
-  unsigned int result = 0;
+	unsigned int result = 0;
 	int i;
-  /* Check if the input is NULL */
-	if (b == NULL) {
+	/* Check if the input is NULL */
+	if (b == NULL)
 	return (0);
-	}
 	/* Iterate through each character in the string*/
 	for (i = 0; b[i] != '\0'; i++)
 	{
-	  /* Check if the current character is not '0' or '1'*/
+	/* Check if the current character is not '0' or '1'*/
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-	  /* Shift the current result left by 1 bit and add the current digit*/
+	/* Shift the current result left by 1 bit and add the current digit*/
 			result = result * 2 + (b[i] - '0');
 	}
 
